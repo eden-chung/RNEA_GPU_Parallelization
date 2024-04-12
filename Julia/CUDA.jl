163149fb32bf895074debbfac2ef34604823df77
eden-chung <edenchung23@gmail.com>
2024-04-12 19:49:33 -0400

    try to add julia cuda stuff

--- Julia/CUDA.jl
+++ Julia/CUDA.jl
@@ -1,16 +1,24 @@
-function time_matmul_julia(size)
-    A = rand(size, size)
-    B = rand(size, size)
-    start_time = time()  # Capture the start time
-    C = A * B  # Perform matrix multiplication
-    end_time = time()  # Capture the end time
-    return end_time - start_time  # Calculate the elapsed time
-end
+using Pkg
+Pkg.add("CUDA")
 
-sizes = [100, 300, 500, 700, 900, 1300, 2000, 4000, 6000, 8000]
-julia_times = [time_matmul_julia(size) for size in sizes]
+using CUDA
+CUDA.versioninfo()
 
-# Print the results
-for (i, size) in enumerate(sizes)
-    println("Matrix size: $size x $size, Time taken: $(julia_times[i]) seconds")
-end
+
+
+# function time_matmul_julia(size)
+#     A = rand(size, size)
+#     B = rand(size, size)
+#     start_time = time()  # Capture the start time
+#     C = A * B  # Perform matrix multiplication
+#     end_time = time()  # Capture the end time
+#     return end_time - start_time  # Calculate the elapsed time
+# end
+
+# sizes = [100, 300, 500, 700, 900, 1300, 2000, 4000, 6000, 8000]
+# julia_times = [time_matmul_julia(size) for size in sizes]
+
+# # Print the results
+# for (i, size) in enumerate(sizes)
+#     println("Matrix size: $size x $size, Time taken: $(julia_times[i]) seconds")
+# end
